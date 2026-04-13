dbf62b95598de77b0c08bf2a7280e543d8d41018
allinrugg-afk <allinrugg@gmail.com>
2026-04-13 20:08:00 +0700

    Update ips.py

--- ips.py
+++ ips.py
@@ -24,7 +24,7 @@ except ImportError:
         RESET_ALL = BRIGHT = ""
 
 # ─── CONFIG ──────────────────────────────────────────────────────────────────
-BASE_PREFIX  = "876fa2cd825d29262ef0__cr.id"
+BASE_PREFIX  = "b923264838c9f9016a12__cr.id"
 NEVA_URL     = "https://nevacloud.com/tools/check-ip/"
 IPAPI_URL    = "http://ip-api.com/json/{ip}?fields=status,regionName,city,as,message,countryCode"
 TIMEOUT      = 10
